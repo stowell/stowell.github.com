f2aee4447e144bd354da769031e9572285f5d2ee
Dan Stowell <dan@stowell.de>
2014-01-02 16:06:07 -0500

    oauth

--- console/tokens.js
+++ console/tokens.js
@@ -2,5 +2,6 @@ var tokens = {
   'localhost': 'GAlO8Ou______3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dWxvY2FsaG9zdLdLSB70cWKIF6Rfx27MQ4Q=',
   'rdio.github.com': 'GA9O8Ou5_____3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dXJkaW8uZ2l0aHViLmNvbW2v8A-b5v65KiNTkYiiReg=',
   'rdio.github.io': 'GA5RZGc-_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnJkaW8uZ2l0aHViLmlvfSrb4OkirIFEIwe9aWFw8Q==',
-  'danstowell.com': 'GA5SxdL2AB4WoWR2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbmRhbnN0b3dlbGwuY29tOHemtY-OBsFgBkjpTIUsIw='
+  //console 'danstowell.com': 'GA5SxdL2AB4WoWR2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbmRhbnN0b3dlbGwuY29tOHemtY-OBsFgBkjpTIUsIw='
+  'danstowell.com': 'GA5SxdR-_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2RhbnN0b3dlbGwuY29tvveKUu3bI8eOkBZChO2EBg=='
 };
