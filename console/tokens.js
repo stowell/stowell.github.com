d9588efa1c94eaf7c36b014d442997e99a3a933e
Dan Stowell <dan@stowell.de>
2014-01-13 10:42:57 -0500

    tokens for bos-vs028 and localhost

--- console/tokens.js
+++ console/tokens.js
@@ -1,3 +1,6 @@
 var tokens = {
-  'danstowell.com': 'GA5SxdR-_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2RhbnN0b3dlbGwuY29tvveKUu3bI8eOkBZChO2EBg=='
+  'danstowell.com': 'GA5SxdR-_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2RhbnN0b3dlbGwuY29tvveKUu3bI8eOkBZChO2EBg==',
+  'bos-vs028': 'GAlS1ADs_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2Jvcy12czAyOBEDtnQmpivAp7gZ__neRLs=',
+  'bos-vs028.p.echonest.net': 'GBhS1AEd_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2Jvcy12czAyOC5wLmVjaG9uZXN0Lm5ldIiUT2xV48n7BE1hiihFcaM=',
+  'localhost': 'GAlS1AFD_____2Nyd2I5N3B4ajJ4YmUyYnI0cm1mZHZqd2xvY2FsaG9zdHPluvlSITCwpu3gpBl50po='
 };
